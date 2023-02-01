688fba96297ffe1db550436406814e4d1f1b21eb
Ganesan Umanesan <ganesan.umanesan@seagate.com>
2023-02-01 13:37:06 +0000

    disks commandline arg added.

--- disks.sh
+++ disks.sh
@@ -19,7 +19,8 @@
 
 #!/bin/bash 
 dest="$HOME/var/motr"
-mkdir -p "$dest" 
-for i in {0..9}; do 
+numd=${1:-10}
+mkdir -p "$dest"
+for((i=0; i<"$numd"; i++)) do 
 	dd if=/dev/zero of="$dest/disk$i.img" bs=1M seek=9999 count=1 
 done
