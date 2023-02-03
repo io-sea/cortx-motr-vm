fe7002863e7e96420612afe7691a4ba5622b3784
Ganesan Umanesan <ganesan.umanesan@seagate.com>
2023-02-03 11:23:42 +0000

    minor fix

--- shutdown-motr-f.sh
+++ shutdown-motr-f.sh
@@ -5,8 +5,8 @@ sudo pkill -9 m0d
 sudo pkill -9 hax
 sudo pkill -9 consul
 sudo hctl shutdown
-sudo du -hcs /var/motr/ /var/log/motr/ /var/log/seagate/
+sudo du -hcs /var/motr/ /var/log/motr/ /var/log/seagate/ 2> /dev/null
 sudo rm -rf /var/motr/
-sudo du -hcs /var/motr/ /var/log/motr/ /var/log/seagate/
+sudo du -hcs /var/motr/ /var/log/motr/ /var/log/seagate/ 2> /dev/null
 echo Done
 
