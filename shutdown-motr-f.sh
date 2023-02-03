1c575b4ae0f994d66564244492d96526c5880dda
Ganesan Umanesan <ganesan.umanesan@seagate.com>
2023-02-03 11:17:55 +0000

    clean up /var/motr/ after shutdown

--- shutdown-motr-f.sh
+++ shutdown-motr-f.sh
@@ -5,5 +5,8 @@ sudo pkill -9 m0d
 sudo pkill -9 hax
 sudo pkill -9 consul
 sudo hctl shutdown
+sudo du -hcs /var/motr/ /var/log/motr/ /var/log/seagate/
+sudo rm -rf /var/motr/
+sudo du -hcs /var/motr/ /var/log/motr/ /var/log/seagate/
 echo Done
 
